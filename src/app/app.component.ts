7e770a67acbc339119679339da0cca952e7a4f30
shaik <indivarangular@gmail.com>
2023-01-11 23:34:05 +0530

    Intial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'demo';
+export class AppComponent implements OnInit{
+  title = 'Angular-Mat-Tab-With-Components';
+  
+  navLinks: any[];
+  activeLinkIndex = -1; 
+  background = 'red';
+  constructor(private router: Router) {
+    this.navLinks = [
+        {
+            label: 'Test',
+            link: './test',
+            index: 0
+        }, 
+        {
+            label: 'Test1',
+            link: './test1',
+            index: 1
+        }, 
+        {
+            label: 'Test2',
+            link: './test2',
+            index: 2
+        },
+        {
+          label: 'Test3',
+          link: './test3',
+          index: 3
+      } 
+    ];
+}
+
+ngOnInit(): void {
+  this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+  });
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { AppMaterialModule } from './app.material.module';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import { Test3Component } from './test3/test3.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    Test3Component,
+    Test1Component,
+    Test2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTabsModule
+   
   ],
+  exports:[MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
